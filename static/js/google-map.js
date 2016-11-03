a5d7af6efdd4f5cda860e662fcc75668d989b516
Baptiste Rosillon <batros.web@gmail.com>
2016-11-03 13:57:22 +0100

    Implemented nearest station search

--- static/js/google-map.js
+++ static/js/google-map.js
@@ -1,3 +1,5 @@
+var stepCoordonates = [];
+
 function initMap() {
 	// Create a map object and specify the DOM element for display.
 	var map = new google.maps.Map(document.getElementById('map'), {
@@ -10,7 +12,14 @@ function initMap() {
 	parser.parse('library/navigate.kml');
 
 	function createM(placemark) {
-		console.log(placemark);
+		//console.log(placemark);	
+		var tmp = {};
+		
+		tmp['name'] = placemark.name;
+		tmp['lat'] = placemark.latlng.lat();
+		tmp['long'] = placemark.latlng.lng();
+		
+		stepCoordonates.push(tmp);
 
 		var markerOptions = {
 			optimized: false,
@@ -23,69 +32,7 @@ function initMap() {
 
 		google.maps.event.addListener(marker, 'click', function()
 		{
-			var lat = placemark.latlng.lat();
-			var lgt = placemark.latlng.lng();
-			var start = "2016-04-05T14:30:00Z";
-			var end = "2016-04-11T15:30:00Z";
-			$.ajax({
-				url: 'http://api.planetos.com/v1/datasets/noaa_ww3_global_1.25x1d/point?lon='+lgt+'&lat='+lat+'&apikey=019ecd71188a4982a20e1a9717e1ab27',
-				type: 'GET',
-				dataType: 'json',
-				success : function(data){
-					var listStation = data["entries"][0]["data"];
-					var Wind_speed_surface = listStation["Wind_speed_surface"];
-					var Wind_direction_from_which_blowing_surface = listStation["Wind_direction_from_which_blowing_surface"];
-					var vcomponent_of_wind_surface = listStation["v-component_of_wind_surface"];
-					var ucomponent_of_wind_surface = listStation["u-component_of_wind_surface"];
-					var Direction_of_wind_waves_surface = listStation["Direction_of_wind_waves_surface"];
-					var Primary_wave_mean_period_surface = listStation["Primary_wave_mean_period_surface"];
-					var Primary_wave_direction_surface = listStation["Primary_wave_direction_surface"];
-					var Significant_height_of_wind_waves_surface = listStation["Significant_height_of_wind_waves_surface"];
-					var Mean_period_of_wind_waves_surface = listStation["Mean_period_of_wind_waves_surface"];
-					var Secondary_wave_mean_period_surface = listStation["Secondary_wave_mean_period_surface"];
-					var Significant_height_of_combined_wind_waves_and_swell_surface = listStation["Significant_height_of_combined_wind_waves_and_swell_surface"];
-					var Secondary_wave_direction_surface = listStation["Secondary_wave_direction_surface"];
-
-					$(".planet").html(
-						"<li>"+
-						Wind_speed_surface+
-						"</li>"+
-						"<li>"+
-						Wind_direction_from_which_blowing_surface+
-						"</li>"+
-						"<li>"+
-						vcomponent_of_wind_surface+
-						"</li>"+
-						"<li>"+
-						ucomponent_of_wind_surface+
-						"</li>"+
-						"<li>"+
-						Direction_of_wind_waves_surface+
-						"</li>"+
-						"<li>"+
-						Primary_wave_mean_period_surface+
-						"</li>"+
-						"<li>"+
-						Primary_wave_direction_surface+
-						"</li>"+
-						"<li>"+
-						Significant_height_of_wind_waves_surface+
-						"</li>"+
-						"<li>"+
-						Mean_period_of_wind_waves_surface+
-						"</li>"+
-						"<li>"+
-						Secondary_wave_mean_period_surface+
-						"</li>"+
-						"<li>"+
-						Significant_height_of_combined_wind_waves_and_swell_surface+
-						"</li>"+
-						"<li>"+
-						Secondary_wave_direction_surface+
-						"</li>"
-						)
-				}
-			})
+			alert(placemark.name);
 		});
 	}
 }
---
+++ static/js/load-data.js
@@ -0,0 +1,64 @@
+var API_KEY = "92988213ff2b4e209292a44300191d1d";
+
+$(function() {
+    
+    // Get stations from 
+    $.ajax({
+        url: "http://api.planetos.com/v1/datasets/noaa_ndbc_stdmet_stations/stations?apikey="+API_KEY,
+        type: "GET",
+        dataType: "json",
+        success: function(data){
+            var stations = data.station;
+            //Set a defaut high distance
+            var stationsDiff = 100000;
+            var distanceBetween;
+            
+            for (var i = 0; i < stepCoordonates.length; i++) {
+                
+                //Find the nearest station
+                for (var station in stations) {
+                    //Distance between the point and current station
+                    distanceBetween = getDistanceFromLatLonInKm(
+                        stations[station].SpatialExtent.coordinates[1],
+                        stations[station].SpatialExtent.coordinates[0],
+                        stepCoordonates[i].lat,
+                        stepCoordonates[i].long
+                    );             
+                    
+                    if (distanceBetween < stationsDiff) {
+                        stationsDiff = distanceBetween;                   
+                        stepCoordonates[i].station = station;            
+                        //console.log(stationsDiff);
+                    }
+                }
+                
+                console.log(stepCoordonates[i]);
+                
+                //Reset distance
+                stationsDiff = 100000;
+            }
+            
+        },
+        error: function() {
+            console.log("Error on ajax stations query");
+        }
+    });
+});
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+        ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI/180);
+}
